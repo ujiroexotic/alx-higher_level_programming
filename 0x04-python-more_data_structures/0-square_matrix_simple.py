2f017c202649c45a9428a568e1a3c59040068222
ujiroexotic <jiroeruteya@gmail.com>
2023-01-11 03:43:48 -0800

    Task 0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
-
-
+    return [[x ** 2 for x in row] for row in matrix]
