50e9be4984caff951261458d29ffb72680a4ad3f
Ujiro Eruteya <ujiroeruteya@gmail.com>
2023-01-23 18:42:01 +0000

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     count = 0
-     i in range(0, x):
+    for i in range(0, x):
 	 try:
 	     print("{}".format(my_list[i]), end="")
 	     count += 1
