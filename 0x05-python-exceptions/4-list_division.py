71a82d1985833cabf988bde2fe64b248ef5d17d5
Ujiro Eruteya <ujiroeruteya@gmail.com>
2023-01-23 15:51:52 +0000

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length)
+def list_division(my_list_1, my_list_2, list_length):
     div = []
     temp_result = 0
     for i in range(0, list_length):
