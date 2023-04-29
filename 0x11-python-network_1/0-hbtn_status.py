76bf51c731ec5bc14e93d33da385e29842973e14
Ujiro Eruteya <ujiroeruteya@gmail.com>
2023-04-28 18:09:47 -0700

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
